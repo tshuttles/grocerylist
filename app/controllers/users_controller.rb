49ebca9447880fb6bacf77916ac3d319b7a0d4cf
tshuttles <tshuttlesworth@gmail.com>
2019-10-12 18:40:26 -0500

    add signup routes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController 
 
     get '/signup' do 
-        "sign up page"
+        if logged_in?
+            redirect '/items' 
+        else 
+            erb :'/users/signup' 
+        end
+    end 
+
+    post '/signup' do 
+        if params[:username] == "" || params[:password] == ""
+            redirect '/signup' 
+        else 
+            user = User.create(username: params[:username], password: params[:password])
+            redirect '/items' 
+        end 
     end 
 
 end 
