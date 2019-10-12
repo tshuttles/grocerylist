20cde7585f9822623a094e389acbfb183e871bdb
tshuttles <tshuttlesworth@gmail.com>
2019-10-12 18:51:56 -0500

    create logout route for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,31 @@ class UsersController < ApplicationController
         end 
     end 
 
+    get '/login' do 
+        if logged_in? 
+            redirect '/items' 
+        else 
+            erb :'users/login' 
+        end 
+    end 
+
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        if user 
+            session[:user_id] = user.id 
+            redirect '/items' 
+        else 
+            redirect '/login' 
+        end 
+    end 
+
+    get '/logout' do 
+        if logged_in? 
+            session.clear 
+            erb :welcome 
+        else 
+            erb :welcome  
+        end 
+    end 
+
 end 
