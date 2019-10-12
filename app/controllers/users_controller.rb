f981d1c92f39b219c344e5a2210a86cf682e25fe
tshuttles <tshuttlesworth@gmail.com>
2019-10-12 18:33:36 -0500

    create controller files and update model files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController 
+
+    get '/signup' do 
+        "sign up page"
+    end 
+
+end 
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base 
+class Users < ActiveRecord::Base 
     has_many :items 
     has_secure_password
 end 
