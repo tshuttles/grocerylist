c51fff7a3c3af64197f9edcd93e9ef22c05bf28a
tshuttles <tshuttlesworth@gmail.com>
2019-10-13 19:27:13 -0500

    create first items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,12 @@
 class ItemsController < ApplicationController 
 
+    get '/items' do 
+        if logged_in? 
+            @items = current_user.items 
+            erb :'/items/index' 
+        else 
+            redirect '/login' 
+        end 
+    end 
+
 end 
