a4dc0e570597d478f1004683a063fa0bc5ecb820
tshuttles <tshuttlesworth@gmail.com>
2019-11-05 23:09:29 -0600

    answer extra project presentation questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
     has_many :items 
     has_secure_password
 
+    # has_many macro: 
+    def item 
+        @item 
+    end 
+
+    def item=
+        @item = item 
+    end 
+
+    def add_item(item)
+        @item.user_id = self.id  
+    end 
+
 end 
